bcccde44561842b4333f7c01306bb81b583c1235
Prathish S <prathish472@gmail.com>
2026-05-14 18:06:08 +0530

    Update script.js

--- Color-Flipper/script.js
+++ Color-Flipper/script.js
@@ -7,11 +7,13 @@ function change(name)
 }
 function random()
 {
+    
     const red = Math.round(Math.random() * 255)
     const green = Math.round(Math.random() * 255)
     const blue = Math.round(Math.random() * 255)
 
     let na =  `rgb(${red},${green},${blue})`
     body.style.backgroundColor = na;
+    
 
 }
